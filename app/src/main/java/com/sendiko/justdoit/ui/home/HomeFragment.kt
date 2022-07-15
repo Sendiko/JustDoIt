00c524dcc911a0b5a6d220bc52043d8c84b921d5
Sendiko <3103120150@student.smktelkom-pwt.sch.id>
2022-07-15 22:42:14 +0700

    HomeFragment ui update

--- app/src/main/java/com/sendiko/justdoit/ui/home/HomeFragment.kt
+++ app/src/main/java/com/sendiko/justdoit/ui/home/HomeFragment.kt
@@ -4,10 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.sendiko.justdoit.databinding.FragmentHomeBinding
+import com.sendiko.justdoit.model.Task
 
 class HomeFragment : Fragment() {
 
@@ -23,16 +24,30 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+            ViewModelProvider(this)[HomeViewModel::class.java]
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupRecyclerView()
+    }
+
+    private fun setupRecyclerView(){
+        val taskList = arrayListOf<Task>(
+            Task(
+                "Publish an android app",
+                "My career path",
+                false
+            )
+        )
+        val rv = binding.rvTask
+        rv.layoutManager = LinearLayoutManager(context)
+        rv.setHasFixedSize(true)
+        rv.adapter = TaskAdapter(taskList)
     }
 
     override fun onDestroyView() {
