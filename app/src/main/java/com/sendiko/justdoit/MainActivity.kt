1d9236a6c5e948c6398c92d396d0dd77636b50d1
Sendiko <3103120150@student.smktelkom-pwt.sch.id>
2022-07-15 14:03:34 +0700

    finished login and register

--- app/src/main/java/com/sendiko/justdoit/MainActivity.kt
+++ app/src/main/java/com/sendiko/justdoit/MainActivity.kt
@@ -1,13 +1,12 @@
-package com.sendiko.jusdoit
+package com.sendiko.justdoit
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.sendiko.jusdoit.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.sendiko.justdoit.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,16 +19,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val navView: BottomNavigationView = binding.navView
-
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
+
         navView.setupWithNavController(navController)
+        navView.itemActiveIndicatorColor = getColorStateList(R.color.lighter_ming)
+
+        navController.addOnDestinationChangedListener{_, destination, _ ->
+            when(destination.id){
+                R.id.loginFragment -> navView.isVisible = false
+                R.id.registerFragment -> navView.isVisible = false
+                R.id.splashScreenFragment -> navView.isVisible = false
+                else -> navView.isVisible = true
+            }
+        }
     }
 }
--- app/src/main/java/com/sendiko/justdoit/ui/register/RegisterFragment.kt
+++ app/src/main/java/com/sendiko/justdoit/ui/register/RegisterFragment.kt
@@ -1,29 +1,32 @@
-package com.sendiko.jusdoit.ui.login
+package com.sendiko.justdoit.ui.register
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.sendiko.jusdoit.R
-import com.sendiko.jusdoit.databinding.FragmentLoginBinding
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.sendiko.justdoit.R
+import com.sendiko.justdoit.databinding.FragmentRegisterBinding
 
-class LoginFragment : Fragment() {
+class RegisterFragment : Fragment() {
 
-    private var _binding : FragmentLoginBinding?= null
+    private var _binding : FragmentRegisterBinding?= null
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentLoginBinding.inflate(layoutInflater)
+        _binding = FragmentRegisterBinding.inflate(layoutInflater)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        // TODO : CODE HERE
+        binding.loginHere.setOnClickListener {
+            findNavController().navigate(R.id.action_registerFragment_to_loginFragment)
+        }
     }
 
     override fun onDestroy() {
