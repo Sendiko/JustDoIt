7cf12288e05971a53ea9b94f18cec0629848af97
Sendiko <3103120150@student.smktelkom-pwt.sch.id>
2022-07-17 07:16:54 +0700

    delete room db files

--- app/src/main/java/com/sendiko/justdoit/database/Constants.kt
+++
@@ -1,8 +0,0 @@
-package com.sendiko.justdoit.database
-
-class Constants {
-    companion object{
-        const val tableName = "TABLE_TASKS"
-        const val databaseName = "TASK_DB"
-    }
-}
--- app/src/main/java/com/sendiko/justdoit/database/TaskDao.kt
+++
@@ -1,7 +0,0 @@
-package com.sendiko.justdoit.database
-
-import androidx.room.Dao
-
-@Dao
-class TaskDao {
-}
