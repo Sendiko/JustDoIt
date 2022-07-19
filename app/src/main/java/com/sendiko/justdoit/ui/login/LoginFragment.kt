12a9e6c2d6041b01336705a804686b6e41fafb1b
Sendiko <3103120150@student.smktelkom-pwt.sch.id>
2022-07-19 14:56:31 +0700

    validation

--- app/src/main/java/com/sendiko/justdoit/ui/login/LoginFragment.kt
+++ app/src/main/java/com/sendiko/justdoit/ui/login/LoginFragment.kt
@@ -25,11 +25,49 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.buttonLogin.setOnClickListener {
-            findNavController().navigate(R.id.action_loginFragment_to_navigation_home)
+            val u = binding.inputUsername.text.toString()
+            val p = binding.inputUsername.text.toString()
+            when{
+                validation(u, p) -> findNavController().navigate(R.id.action_loginFragment_to_navigation_home)
+            }
         }
         binding.textView.setOnClickListener {
             findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
         }
+    }
+
+    private fun validation(username : String, password : String): Boolean {
+        var valid = true
+        when {
+            username.isNotBlank() -> {
+                when {
+                    password.isNotBlank() -> {
+                        valid = true
+                        binding.layoutUsername.error = null
+                        binding.layoutPassword.error = null
+                    }
+                    username.isBlank() || password.isBlank() -> {
+                        valid = false
+                        binding.layoutUsername.error = "Please fill in Username and Password"
+                        binding.layoutPassword.error = "Please fill in Username and Password"
+                    }
+                    password.length < 8 -> {
+                        binding.layoutPassword.error = "Password must be at least 8 characters"
+                        valid = false
+                    }
+                }
+            }
+            username.isBlank() || password.isBlank() -> {
+                valid = false
+                binding.layoutUsername.error = "Please fill in Username and Password"
+                binding.layoutPassword.error = "Please fill in Username and Password"
+            }
+            password.length < 8 -> {
+                binding.layoutPassword.error = "Password must be at least 8 characters"
+                valid = false
+            }
+        }
+        return valid
     }
 
     override fun onDestroy() {
--- app/src/main/java/com/sendiko/justdoit/ui/register/RegisterFragment.kt
+++ app/src/main/java/com/sendiko/justdoit/ui/register/RegisterFragment.kt
@@ -27,6 +27,46 @@ class RegisterFragment : Fragment() {
         binding.loginHere.setOnClickListener {
             findNavController().navigate(R.id.action_registerFragment_to_loginFragment)
         }
+
+        binding.buttonRegister.setOnClickListener {
+            val u = binding.inputUsername.text.toString()
+            val e = binding.inputEmail.text.toString()
+            val p = binding.inpurPassword.toString()
+            val cp = binding.inputConfirmPassword.toString()
+            when{
+                validation(u, e, p, cp) -> findNavController().navigate(R.id.action_registerFragment_to_loginFragment)
+            }
+        }
+
+    }
+
+    private fun validation(u: String, e : String, p: String, cp : String): Boolean {
+        var valid = true
+        when{
+            u.isEmpty() -> {
+                binding.layoutUsername.error = "Username can't be empty"
+                valid = false
+            }
+            e.isEmpty() -> {
+                binding.layoutEmail.error = "Email can't be empty"
+                valid = false
+            }
+            p.isEmpty() -> {
+                binding.layoutPassword.error = "Password can't be empty"
+                valid = false
+            }
+            p.length < 8 -> {
+                binding.layoutPassword.error = "Password need to be at least 8 characters"
+                valid = false
+            }
+            p != cp -> {
+                binding.layoutPassword.error = "Password didn't match"
+                binding.layoutConfirm.error = "Password didn't match"
+                valid = false
+            }
+            else -> valid = true
+        }
+        return valid
     }
 
     override fun onDestroy() {
