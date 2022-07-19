0f5242e954b38be8298492c99041eb407d7bb238
Sendiko <3103120150@student.smktelkom-pwt.sch.id>
2022-07-19 11:40:18 +0700

    adjust SplashScreen

--- app/src/main/java/com/sendiko/justdoit/ui/splash/SplashScreenFragment.kt
+++ app/src/main/java/com/sendiko/justdoit/ui/splash/SplashScreenFragment.kt
@@ -3,10 +3,11 @@ package com.sendiko.justdoit.ui.splash
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.sendiko.justdoit.R
 import com.sendiko.justdoit.databinding.FragmentSplashScreenBinding
@@ -21,13 +22,25 @@ class SplashScreenFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentSplashScreenBinding.inflate(layoutInflater)
+
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val zoomOut = AnimationUtils.loadAnimation(context, R.anim.zoom_out)
+        val zoomIn = AnimationUtils.loadAnimation(context, R.anim.zoom_in)
+        binding.appName.startAnimation(zoomOut)
+        binding.appLogo.startAnimation(zoomOut)
+
+        Handler(Looper.myLooper()!!).postDelayed({
+            binding.appName.startAnimation(zoomIn)
+            binding.appLogo.startAnimation(zoomIn)
+        }, 100)
+
         Handler(Looper.myLooper()!!).postDelayed({
             findNavController().navigate(R.id.action_splashScreenFragment_to_loginFragment)
         }, 1000)
+
     }
 }
