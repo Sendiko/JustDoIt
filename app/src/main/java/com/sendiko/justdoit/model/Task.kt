18fc9dcc3d548d8e29d803bf27d0a13533606992
Sendiko <3103120150@student.smktelkom-pwt.sch.id>
2022-07-20 15:02:37 +0700

    create to db

--- app/src/main/java/com/sendiko/justdoit/model/Task.kt
+++ app/src/main/java/com/sendiko/justdoit/model/Task.kt
@@ -1,7 +1,6 @@
 package com.sendiko.justdoit.model
 
 data class Task(
-   val id : Int?,
    var task : String,
    val subject: String?,
    val isDone : Boolean
--- app/src/main/java/com/sendiko/justdoit/ui/task/TaskFragment.kt
+++ app/src/main/java/com/sendiko/justdoit/ui/task/TaskFragment.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
 import com.sendiko.justdoit.R
 import com.sendiko.justdoit.databinding.FragmentTaskBinding
 
@@ -13,6 +15,8 @@ class TaskFragment : Fragment() {
 
    private var _binding : FragmentTaskBinding?= null
    private val binding get() = _binding!!
+
+   private val taskViewModel : TaskViewModel by activityViewModels()
 
    override fun onCreateView(
       inflater: LayoutInflater, container: ViewGroup?,
@@ -28,6 +32,18 @@ class TaskFragment : Fragment() {
 
       binding.icCancel.setOnClickListener {
          findNavController().navigate(R.id.action_taskFragment2_to_navigation_home)
+      }
+
+      binding.buttonSave.setOnClickListener {
+         val t = binding.inputTask.text.toString()
+         val s = binding.inputSubject.text.toString()
+         taskViewModel.insertTask(t, s)
+         taskViewModel.success.observe(viewLifecycleOwner){
+            when{
+               it -> findNavController().navigate(R.id.action_taskFragment2_to_navigation_home)
+               else -> Snackbar.make(requireView(), "Error", Snackbar.LENGTH_LONG).show()
+            }
+         }
       }
 
    }
