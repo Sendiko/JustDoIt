29e7b3d2eef89d704241d0475f5c6689cc502456
Sendiko <3103120150@student.smktelkom-pwt.sch.id>
2022-07-22 08:29:37 +0700

    declare and obtain homeviewmodel

--- app/src/main/java/com/sendiko/justdoit/ui/home/HomeFragment.kt
+++ app/src/main/java/com/sendiko/justdoit/ui/home/HomeFragment.kt
@@ -1,11 +1,13 @@
 package com.sendiko.justdoit.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
@@ -14,16 +16,18 @@ import com.sendiko.justdoit.R
 import com.sendiko.justdoit.dataStore1
 import com.sendiko.justdoit.databinding.FragmentHomeBinding
 import com.sendiko.justdoit.model.Task
+import com.sendiko.justdoit.repository.ViewModelFactory
 import com.sendiko.justdoit.repository.preferences.AuthPreferences
 import com.sendiko.justdoit.repository.preferences.AuthViewModel
 import com.sendiko.justdoit.repository.preferences.AuthViewModelFactory
+import com.sendiko.justdoit.ui.dashboard.DashboardViewModel
 
 class HomeFragment : Fragment() {
 
    private var _binding: FragmentHomeBinding? = null
    private val binding get() = _binding!!
 
-   private val homeViewModel : HomeViewModel by activityViewModels()
+   private lateinit var task : ArrayList<Task>
 
    private val pref by lazy{
       val context = requireNotNull(this.context)
@@ -32,6 +36,16 @@ class HomeFragment : Fragment() {
 
    private val authViewModel : AuthViewModel by lazy {
       ViewModelProvider(this, AuthViewModelFactory(pref))[AuthViewModel::class.java]
+   }
+
+   private val homeViewModel by lazy {
+      val activity = requireNotNull(this.activity)
+      getViewModel(activity)
+   }
+
+   private fun getViewModel(activity: FragmentActivity) :HomeViewModel {
+      val factory = ViewModelFactory.getInstance(activity.application)
+      return ViewModelProvider(this, factory)[HomeViewModel::class.java]
    }
 
    override fun onCreateView(
