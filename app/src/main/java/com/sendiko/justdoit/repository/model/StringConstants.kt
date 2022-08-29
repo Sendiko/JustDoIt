d1eb92a90f564204d65ab386a7bef71a2802fc7a
Sendiko <3103120150@student.smktelkom-pwt.sch.id>
2022-08-29 10:10:43 +0700

    added StringConstants.kt

---
+++ app/src/main/java/com/sendiko/justdoit/repository/model/StringConstants.kt
@@ -0,0 +1,20 @@
+package com.sendiko.justdoit.repository.model
+
+class StringConstants {
+    companion object {
+        const val falsee = "false"
+        const val truee = "true"
+        const val update = "Update"
+        const val updateTask = "Update Task"
+        const val backToast = "use home button to exit the app"
+        const val categoriesSport = "Sport"
+        const val categoriesWork = "Work"
+        const val categoriesGame = "Game"
+        const val categoriesSchool = "School"
+        const val categoriesChores = "Chores"
+        const val emptyError = "Task can't be empty"
+        const val usernameError = "Username can't be empty"
+        const val dbName = "task_database"
+        const val unknownModelClass = "Unknown model class : "
+    }
+}
