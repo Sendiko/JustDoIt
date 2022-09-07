9d063851b2b0e75b05e81b0d732b3a2108753136
Sendiko <3103120150@student.smktelkom-pwt.sch.id>
2022-09-07 10:24:15 +0700

    redesign setting fragment

--- app/src/main/java/com/sendiko/justdoit/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/sendiko/justdoit/ui/settings/SettingsFragment.kt
@@ -1,13 +1,22 @@
 package com.sendiko.justdoit.ui.settings
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.RadioGroup
+import android.widget.Toast
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+import com.sendiko.justdoit.R
 import com.sendiko.justdoit.databinding.FragmentSettingsBinding
 import com.sendiko.justdoit.repository.SharedViewModel
 import com.sendiko.justdoit.repository.preferences.AuthPreferences
@@ -54,7 +63,7 @@ class SettingsFragment : Fragment() {
          binding.greeting3.text = it
       }
 
-      binding.icLogout.setOnClickListener {
+      binding.frameLogout.setOnClickListener {
          authViewModel.setLoginState(false)
          authViewModel.clearUser()
          sharedViewModel.removeUsername()
@@ -62,6 +71,85 @@ class SettingsFragment : Fragment() {
          startActivity(intent)
       }
 
+      binding.frameContacts.setOnClickListener {
+         val link = "https://github.com/Sendiko/JustDoIt/issues"
+         link.openLink()
+      }
+
+      binding.frameLanguages.setOnClickListener {
+         showLanguageDialog()
+      }
+
+      binding.frameDisplayMode.setOnClickListener {
+         showDisplayDialog()
+      }
+
+      binding.frameChangeName.setOnClickListener {
+         showChangeNameDialog()
+      }
+
+   }
+
+   private fun showLanguageDialog(){
+      val languageDialog = BottomSheetDialog(requireContext(), R.style.BottomSheetDialogTheme)
+      val view = layoutInflater.inflate(R.layout.fragment_language_dialog, null)
+      languageDialog.setContentView(view)
+      languageDialog.show()
+
+      val radioGroup = view.findViewById<RadioGroup>(R.id.languages_checkbox)
+      radioGroup.setOnCheckedChangeListener { _, i ->
+         when(i){
+            R.id.language_english -> Toast.makeText(context, "English", Toast.LENGTH_SHORT)
+               .show()
+            R.id.language_indonesian -> Toast.makeText(context, "Indonesian", Toast.LENGTH_SHORT)
+               .show()
+         }
+      }
+
+   }
+
+   private fun showDisplayDialog(){
+      val displayDialog = BottomSheetDialog(requireContext(), R.style.BottomSheetDialogTheme)
+      val view = layoutInflater.inflate(R.layout.fragment_display_dialog, null)
+      displayDialog.setContentView(view)
+      displayDialog.show()
+
+      val radioGroup = view.findViewById<RadioGroup>(R.id.display_checkbox)
+      radioGroup.setOnCheckedChangeListener { _, i ->
+         when(i){
+            R.id.theme_light -> Toast.makeText(context, "Light", Toast.LENGTH_SHORT)
+               .show()
+            R.id.theme_dark -> Toast.makeText(context, "Dark", Toast.LENGTH_SHORT)
+               .show()
+         }
+      }
+   }
+
+   private fun showChangeNameDialog(){
+      val changeNameDialog = BottomSheetDialog(requireContext(), R.style.BottomSheetDialogTheme)
+      val view = layoutInflater.inflate(R.layout.fragment_change_name_dialog, null)
+      changeNameDialog.setContentView(view)
+      changeNameDialog.show()
+
+      val layoutName = view.findViewById<TextInputLayout>(R.id.layout_name)
+      val inputName = view.findViewById<TextInputEditText>(R.id.input_name)
+      val buttonSubmit = view.findViewById<Button>(R.id.button_submit)
+      buttonSubmit.setOnClickListener {
+         when{
+            inputName.text.isNullOrEmpty() -> {
+               inputName?.background = AppCompatResources.getDrawable(requireContext(), R.drawable.box_background_error)
+               layoutName.error = "This can't be empty"
+            }
+            else -> Toast.makeText(context, "${inputName.text}", Toast.LENGTH_SHORT).show()
+         }
+      }
+
+   }
+
+   private fun String.openLink() {
+      val intent = Intent(Intent.ACTION_VIEW)
+      intent.data = Uri.parse(this)
+      requireActivity().startActivity(intent)
    }
 
    override fun onDestroyView() {
