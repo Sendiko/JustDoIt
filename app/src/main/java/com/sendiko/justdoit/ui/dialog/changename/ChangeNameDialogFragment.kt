f3b6e5c202cd6889bc47b00701c455c5097f53c5
Sendiko <3103120150@student.smktelkom-pwt.sch.id>
2022-09-07 10:27:03 +0700

    add display, language, change name fragment

---
+++ app/src/main/java/com/sendiko/justdoit/ui/dialog/changename/ChangeNameDialogFragment.kt
@@ -0,0 +1,34 @@
+package com.sendiko.justdoit.ui.dialog.changename
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.sendiko.justdoit.databinding.FragmentChangeNameDialogBinding
+
+class ChangeNameDialogFragment : BottomSheetDialogFragment() {
+
+    private var _binding: FragmentChangeNameDialogBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentChangeNameDialogBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // TODO: CODE HERE
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
