5bf8e87f409a31064ffd8c1a2779e215b62d4b39
Sendiko <3103120150@student.smktelkom-pwt.sch.id>
2022-09-08 19:00:51 +0700

    added SettingsPreference.kt and SettingsViewModel.kt

---
+++ app/src/main/java/com/sendiko/justdoit/ui/settings/SettingsViewModel.kt
@@ -0,0 +1,38 @@
+package com.sendiko.justdoit.ui.settings
+
+import androidx.lifecycle.*
+import com.sendiko.justdoit.repository.preferences.SettingsPreference
+import kotlinx.coroutines.launch
+
+class SettingsViewModel(private val settings : SettingsPreference) : ViewModel() {
+
+    fun setDarkTheme(darkTheme : Boolean){
+        viewModelScope.launch {
+            settings.setDarkTheme(darkTheme)
+        }
+    }
+
+    fun getDarkTheme() : LiveData<Boolean> {
+        return settings.getDarkTheme().asLiveData()
+    }
+
+    fun setLanguage(language : String){
+        viewModelScope.launch {
+            settings.setLanguageKey(language)
+        }
+    }
+
+    fun getLanguage() : LiveData<String> {
+        return settings.getLanguageKey().asLiveData()
+    }
+
+    class SettingsViewModelFactory(private val settings : SettingsPreference) : ViewModelProvider.NewInstanceFactory(){
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            when{
+                modelClass.isAssignableFrom(SettingsViewModel::class.java) -> return SettingsViewModel(settings) as T
+                else -> throw IllegalArgumentException("Unknown model class : " + modelClass.name)
+            }
+        }
+    }
+
+}
